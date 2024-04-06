d55f2b9668b65d792f7548942b554f743132d075
spiderMonk <gauravmaharaj1234@gmail.com>
2024-04-07 04:07:43 +0530

    updated app and index, f-commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {BrowserRouter as Router,Routes,Route}from "react-router-dom";
+import Header from "./component/Header"
+import Home from "./component/Home";
+
+import CoinsDetail from "./component/CoinsDetail";
+import Exchanges from "./component/Exchanges";
+import Coin from "./component/Coin";
+import Footer from "./component/Footer";
+
+
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+   <Router>
+    <Header/>
+    <Routes>
+      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/coins"} element={<Coin/>}/>
+      <Route path={"/exchanges"} element ={<Exchanges/>}/>
+      <Route path={"/coins/:id"} element={<CoinsDetail/>}/>
+    </Routes>
+    <Footer/>
+   </Router>
   );
 }
 
